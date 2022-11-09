228523cef7b0cae8f810e9ba46d9cfcb2ab6c0a6
VitorSilva11 <svitoryago@hotmail.com>
2022-11-09 15:48:52 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/controllers/PaymentController.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/controllers/PaymentController.java
@@ -3,6 +3,7 @@ package com.devsuperior.hrpayroll.controllers;
 
 import com.devsuperior.hrpayroll.model.Payment;
 import com.devsuperior.hrpayroll.service.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,11 +21,18 @@ public class PaymentController {
         this.paymentService = paymentService;
     }
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping("/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
         Payment payment = paymentService.getPayment(workerId, days);
         return ResponseEntity.ok(payment);
 
     }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days){
+        Payment payment = new Payment("Brann", 400.0, days);
+        return ResponseEntity.ok(payment);
+    }
+
 
 }
